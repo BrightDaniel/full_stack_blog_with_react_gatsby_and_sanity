36400e22cedb5c1e4a63ea13c22c07780968d18d
unknown <brightdaniel5050@gmail.com>
2022-04-26 20:44:51 +0100

    second commit

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import HeroSection from '../components/homepage/HeroSection';
 import SEO from '../components/SEO';
 import FeaturedBlogs from '../components/homepage/FeaturedBlogs';
@@ -10,6 +11,9 @@ const IndexPage = () => (
       title="Home"
       description="Iv Quest blog provides the latest and reliable educative information about various subject, such as: 'How to make money online, Google career job opportunites, Handshake, Relationship and love advice, walmart, target,  long distant relationships, ups etc'"
     />
+    <Helmet>
+      <meta name="icon" href="/link/to/favicon.ico" />
+    </Helmet>
     <HeroSection />
     <div className="container">
       <FeaturedBlogs />
